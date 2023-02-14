4ee2dbe52d98fe7352d760e7858459660fc97e66
Pawan Kumar <pwnsub2001@pec.edu>
2023-02-14 20:39:12 +0530

    edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
     def index
-        @post = Post.all.order("created_at DESC")
+        @posts = Post.all.order("created_at DESC")
     end
 
     def new
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
     end
 
     def create
-        @post = Post.new()
+        @post = Post.new(post_params)
         if @post.save
             redirect_to @post
         else
@@ -20,6 +20,28 @@ class PostsController < ApplicationController
     def show
         @post = Post.find(params[:id])
     end
+
+    def update
+        @post = Post.find(params[:id])
+
+        if @post.update(post_params)
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect_to @posts_path
+    end
+    
     private
 
     def post_params
